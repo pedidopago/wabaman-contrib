e5dff2b56997a3bafb6395495c7106312f0228ac
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2022-10-04 11:05:45 -0300

    update contact

--- rest/client/client.go
+++ rest/client/client.go
@@ -29,16 +29,32 @@ func (c *Client) NewMessage(ctx context.Context, req *rest.NewMessageRequest) (*
 	return output, nil
 }
 
+func (c *Client) UpdateContact(ctx context.Context, contactID uint64, req *rest.UpdateContactRequest) (*rest.UpdateClientResponse, error) {
+	resp := &rest.UpdateClientResponse{}
+	if err := c.put(ctx, fmt.Sprintf("/api/v1/contact/%d", contactID), req, resp); err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
 func (c *Client) urlPrefix() string {
 	return util.Default(c.BaseURL, DefaultBaseURL)
 }
 
+func (c *Client) put(ctx context.Context, suffix string, input, output any) error {
+	return c.postOrPut(ctx, http.MethodPut, suffix, input, output)
+}
+
 func (c *Client) post(ctx context.Context, suffix string, input, output any) error {
+	return c.postOrPut(ctx, http.MethodPost, suffix, input, output)
+}
+
+func (c *Client) postOrPut(ctx context.Context, method, suffix string, input, output any) error {
 	d, err := json.Marshal(input)
 	if err != nil {
 		return fmt.Errorf("marshal input: %w", err)
 	}
-	req, err := http.NewRequest(http.MethodPost, c.urlPrefix()+suffix, bytes.NewReader(d))
+	req, err := http.NewRequest(method, c.urlPrefix()+suffix, bytes.NewReader(d))
 	if err != nil {
 		return fmt.Errorf("new request: %w", err)
 	}
--- rest/types.go
+++ rest/types.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/pedidopago/wabaman-contrib/fbgraph"
 )
@@ -51,6 +52,14 @@ type NewMediaResponse struct {
 	MediaID string `json:"media_id"`
 }
 
+type UpdateContactRequest struct {
+	CustomerID string `json:"customer_id"`
+}
+
+type UpdateClientResponse struct {
+	Contact *Contact `json:"contact"`
+}
+
 type ErrorResponse struct {
 	Message    string `json:"message"`
 	StatusCode int    `json:"status_code,omitempty"`
@@ -71,4 +80,22 @@ func (e *ErrorResponse) Error() string {
 		return "raw response: " + e.Raw
 	}
 	return "unknown error"
+}
+
+type Contact struct {
+	ID                           uint64    `json:"id,omitempty"`                              // "id": 1,
+	PhoneID                      uint      `json:"phone_id,omitempty"`                        // "phone_id": 1,
+	WabaContactID                string    `json:"waba_contact_id,omitempty"`                 // "waba_contact_id": "5511941011935",
+	WabaProfileName              string    `json:"waba_profile_name,omitempty"`               // "waba_profile_name": "Gabriel Ochsenhofer",
+	LastActivity                 time.Time `json:"last_activity,omitempty"`                   // "last_activity": "2022-10-03T19:32:19Z",
+	LastMessageReceivedID        uint64    `json:"last_message_received_id,omitempty"`        // "last_message_received_id": 145,
+	LastMessageSentId            uint64    `json:"last_message_sent_id,omitempty"`            // "last_message_sent_id": 918,
+	LastMessageReceivedTimestamp time.Time `json:"last_message_received_timestamp,omitempty"` // "last_message_received_timestamp": "2022-10-03T19:32:19Z",
+	LastMessageSentTimestamp     time.Time `json:"last_message_sent_timestamp,omitempty"`     // "last_message_sent_timestamp": "2022-10-04T02:30:23Z",
+	CustomerID                   string    `json:"customer_id,omitempty"`                     // "customer_id": "01F5E1TNWH1TCTGJ1VW71X1NA8",
+	CreatedAt                    time.Time `json:"created_at,omitempty"`                      // "created_at": "2022-09-02T14:04:08Z",
+	UpdatedAt                    time.Time `json:"updated_at,omitempty"`                      // "updated_at": "2022-10-04T02:30:26Z",
+	//TODO: add fields below
+	// HostMessages string `json:"host_messages,omitempty"` // "host_messages": null,
+	// ClientMessages string `json:"client_messages,omitempty"` // "client_messages": null
 }
