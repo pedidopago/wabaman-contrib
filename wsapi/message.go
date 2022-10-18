e2be92f3a442311d1965c93ede05d09a92754d7f
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2022-10-18 13:57:15 -0300

    add receipts

--- wsapi/message.go
+++ wsapi/message.go
@@ -1,5 +1,7 @@
 package wsapi
 
+import "time"
+
 // MessageType is the identifier of the Message payload.
 type MessageType uint8
 
@@ -9,18 +11,22 @@ const (
 	MessageTypePong               MessageType = 1
 	MessageTypeClientMessage      MessageType = 2
 	MessageTypeHostMessage        MessageType = 3
+	MessageTypeReadByHostReceipt  MessageType = 4
+	MessageTypeClientReceipt      MessageType = 5
 	MessageTypeMockClientMessages MessageType = 230
 	MessageTypeCloseError         MessageType = 240
 )
 
 // Message is the root level object of a ms-wabaman websocket.
 type Message struct {
-	Type           MessageType     `json:"type"`
-	Error          *Error          `json:"error,omitempty"`
-	ClientMessage  *ClientMessage  `json:"client_message,omitempty"`
-	HostMessage    *HostMessage    `json:"host_message,omitempty"`
-	Metadata       *Metadata       `json:"metadata,omitempty"`
-	ClientMockData *ClientMockData `json:"client_mock_data,omitempty"`
+	Type              MessageType        `json:"type"`
+	Error             *Error             `json:"error,omitempty"`
+	ClientMessage     *ClientMessage     `json:"client_message,omitempty"`
+	HostMessage       *HostMessage       `json:"host_message,omitempty"`
+	ReadByHostReceipt *ReadByHostReceipt `json:"read_by_host_receipt,omitempty"`
+	ClientReceipt     *ClientReceipt     `json:"client_receipt,omitempty"`
+	Metadata          *Metadata          `json:"metadata,omitempty"`
+	ClientMockData    *ClientMockData    `json:"client_mock_data,omitempty"`
 }
 
 type ClientMockData struct {
@@ -31,4 +37,14 @@ type ClientMockData struct {
 	HostPhoneNumber   string `json:"host_phone_number"`
 	Type              string `json:"type"`
 	Text              *Text  `json:"text,omitempty"`
+}
+
+type ReadByHostReceipt struct {
+	ReadAt   time.Time `json:"read_at"`
+	Metadata string    `json:"metadata"`
+}
+
+type ClientReceipt struct {
+	Type      string    `json:"type"` // sent, read, delivered
+	UpdatedAt time.Time `json:"updated_at"`
 }
