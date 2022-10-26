4b6f083e28d35c1915fa0197aed877dcf85a93ba
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2022-10-26 12:12:24 -0300

    update types

--- wsapi/hostmessage.go
+++ wsapi/hostmessage.go
@@ -36,12 +36,43 @@ type HostTemplate struct {
 	Original    *TemplateRef            `json:"original"`
 }
 
+type TemplateCategory string
+
+const (
+	TplCategoryTransactional         TemplateCategory = "transactional"
+	TplCategoryMarketing             TemplateCategory = "marketing"
+	TplCategoryDisposableCredentials TemplateCategory = "disposable_credentials"
+)
+
+type TemplateHeaderType string
+
+const (
+	TplHeaderTypeNone  TemplateHeaderType = "none"
+	TplHeaderTypeText  TemplateHeaderType = "text"
+	TplHeaderTypeMedia TemplateHeaderType = "media"
+)
+
+type TemplateButtonsType string
+
+const (
+	TplButtonsTypeNone         TemplateButtonsType = "none"
+	TplButtonsTypeCallToAction TemplateButtonsType = "call_to_action"
+	TplButtonsTypeQuickReply   TemplateButtonsType = "quick_reply"
+)
+
+type TemplateCTAButtonType string
+
+const (
+	TplCTABCall TemplateCTAButtonType = "call"
+	TplCTABURL  TemplateCTAButtonType = "url"
+)
+
 type TemplateRef struct {
-	ID         uint64    `json:"id"`
-	BusinessID uint      `json:"business_id"`
-	Name       string    `json:"name"`
-	Category   string    `json:"category"`
-	CreatedAt  time.Time `json:"created_at"`
+	ID         uint64           `json:"id"`
+	BusinessID uint             `json:"business_id"`
+	Name       string           `json:"name"`
+	Category   TemplateCategory `json:"category"`
+	CreatedAt  time.Time        `json:"created_at"`
 
 	Languages []*TemplateRefLanguage `json:"languages"`
 }
@@ -49,18 +80,18 @@ type TemplateRef struct {
 type TemplateRefLanguage struct {
 	LanguageCode string `json:"language_code"`
 
-	Header      *TplRefHeader `json:"header"`
-	Body        string        `json:"body"`
-	Footer      string        `json:"footer"`
-	ButtonsType string        `json:"buttons_type"`
+	Header      *TplRefHeader       `json:"header"`
+	Body        string              `json:"body"`
+	Footer      string              `json:"footer"`
+	ButtonsType TemplateButtonsType `json:"buttons_type"`
 
-	QuickReplyButtons   []TplQuickReplyButton   `json:"quick_reply_buttons"`
-	CallToActionButtons []TplCallToActionButton `json:"call_to_action_buttons"`
+	QuickReplyButtons   []TplQuickReplyButton   `json:"quick_reply_buttons,omitempty"`
+	CallToActionButtons []TplCallToActionButton `json:"call_to_action_buttons,omitempty"`
 }
 
 type TplRefHeader struct {
-	Type           string `json:"header_type"`
-	ContentExample string `json:"content_example"`
+	Type           TemplateHeaderType `json:"header_type"`
+	ContentExample string             `json:"content_example"`
 }
 
 type TplQuickReplyButton struct {
@@ -68,14 +99,18 @@ type TplQuickReplyButton struct {
 }
 
 type TplCallToActionButton struct {
-	Type string `json:"type"`
-	Text string `json:"text"`
-	URL  struct {
-		Type string `json:"type"` // static,dynamic
-		Href string `json:"href"`
-	} `json:"url,omitempty"`
-	Call struct {
-		CC    string `json:"cc"`
-		Phone string `json:"phone"`
-	} `json:"call,omitempty"`
+	Type string               `json:"type"`
+	Text string               `json:"text"`
+	URL  *TplCallToActionURL  `json:"url,omitempty"`
+	Call *TplCallToActionCall `json:"call,omitempty"`
+}
+
+type TplCallToActionURL struct {
+	Type string `json:"type"` // static,dynamic
+	Href string `json:"href"`
+}
+
+type TplCallToActionCall struct {
+	CC    string `json:"cc"`
+	Phone string `json:"phone"`
 }
