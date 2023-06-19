a8b6ccc9b6c8fe5edace8fc22a78975786f8722c
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2023-06-19 15:07:11 -0300

    better typing

--- event/template.go
+++ event/template.go
@@ -6,11 +6,24 @@ import (
 	"github.com/pedidopago/wabaman-contrib/fbgraph"
 )
 
+type TemplateEventKind string
+
+const (
+	TemplateEventApproved        TemplateEventKind = "APPROVED"
+	TemplateEventDisabled        TemplateEventKind = "DISABLED"
+	TemplateEventInAppeal        TemplateEventKind = "IN_APPEAL"
+	TemplateEventPending         TemplateEventKind = "PENDING"
+	TemplateEventReinstated      TemplateEventKind = "REINSTATED"
+	TemplateEventRejected        TemplateEventKind = "REJECTED"
+	TemplateEventFlagged         TemplateEventKind = "FLAGGED"
+	TemplateEventPendingDeletion TemplateEventKind = "PENDING_DELETION"
+)
+
 type TemplateEvent struct {
 	StoreID    string                   `json:"store_id"`
 	BranchID   string                   `json:"branch_id,omitempty"`
 	PhoneID    uint                     `json:"phone_id,omitempty"`
-	Event      string                   `json:"event,omitempty"`
+	Event      TemplateEventKind        `json:"event,omitempty"`
 	TemplateID string                   `json:"template_id,omitempty"`
 	Template   *fbgraph.MessageTemplate `json:"template,omitempty"`
 }
