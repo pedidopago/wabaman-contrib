09c1d911af8aa9cc498f50ff4586167bc661da5c
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2024-01-26 10:08:30 -0300

    add omitempty

--- wsapi/presence.go
+++ wsapi/presence.go
@@ -1,21 +1,21 @@
 package wsapi
 
 type PresenceViewClient struct {
-	AgentID   string `json:"agent_id"`
-	AgentName string `json:"agent_name"` // optional, but it is required if the agent_id is empty
+	AgentID   string `json:"agent_id,omitempty"`
+	AgentName string `json:"agent_name,omitempty"` // optional, but it is required if the agent_id is empty
 }
 
 type PresenceTypingToClient struct {
-	AgentID   string `json:"agent_id"`
-	AgentName string `json:"agent_name"` // optional, but it is required if the agent_id is empty
+	AgentID   string `json:"agent_id,omitempty"`
+	AgentName string `json:"agent_name,omitempty"` // optional, but it is required if the agent_id is empty
 }
 
 type PresenceAgentViewingClient struct {
-	AgentID   string `json:"agent_id"`
-	AgentName string `json:"agent_name"`
+	AgentID   string `json:"agent_id,omitempty"`
+	AgentName string `json:"agent_name,omitempty"`
 }
 
 type PresenceAgentTypingToClient struct {
-	AgentID   string `json:"agent_id"`
-	AgentName string `json:"agent_name"`
+	AgentID   string `json:"agent_id,omitempty"`
+	AgentName string `json:"agent_name,omitempty"`
 }
