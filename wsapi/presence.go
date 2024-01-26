50ec8739c3974e11cbf43e18fbfe92cd9cb14386
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2024-01-26 09:46:35 -0300

    added optional agent name

--- wsapi/presence.go
+++ wsapi/presence.go
@@ -1,11 +1,13 @@
 package wsapi
 
 type PresenceViewClient struct {
-	AgentID string `json:"agent_id"`
+	AgentID   string `json:"agent_id"`
+	AgentName string `json:"agent_name"` // optional, but it is required if the agent_id is empty
 }
 
 type PresenceTypingToClient struct {
-	AgentID string `json:"agent_id"`
+	AgentID   string `json:"agent_id"`
+	AgentName string `json:"agent_name"` // optional, but it is required if the agent_id is empty
 }
 
 type PresenceAgentViewingClient struct {
