8e4f314836c8520d4f5e447a34bfb54ef5822787
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2025-05-02 15:17:29 -0300

    Enhance SafeMetadata with improved JSON marshaling and unmarshaling for better handling of null and raw byte data

--- shared-types/metadata.go
+++ shared-types/metadata.go
@@ -2,20 +2,29 @@ package types
 
 import (
 	"encoding/json"
-	"log/slog"
 )
 
 type SafeMetadata json.RawMessage
 
 func (m SafeMetadata) MarshalJSON() ([]byte, error) {
-	d, err := json.Marshal(json.RawMessage(m))
-	if err != nil {
-		slog.Error("SafeMetadata.MarshalJSON: failed to marshal json.RawMessage", slog.Any("err", err), slog.Any("data", string(m)))
-		return nil, nil
+	if m == nil {
+		return []byte("null"), nil
 	}
-	return d, nil
+
+	return []byte(m), nil
 }
 
 func (m *SafeMetadata) UnmarshalJSON(data []byte) error {
-	return json.Unmarshal(data, (*json.RawMessage)(m))
+	if m == nil || data == nil {
+		return nil
+	}
+
+	if string(data) == "null" {
+		return nil
+	}
+
+	*m = make([]byte, len(data))
+	copy((*m)[:], data)
+
+	return nil
 }
