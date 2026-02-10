77e1daad723561b86bb05bd52dd8dc54f3dee412
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2026-02-10 19:25:19 -0300

    deprecated comment

--- wsapi/calls.go
+++ wsapi/calls.go
@@ -16,7 +16,7 @@ type ConnectToCall struct {
 	CallID    string `json:"call_id"`
 	PhoneID   uint   `json:"phone_id"`
 	BranchID  string `json:"branch_id"`
-	SDPType   string `json:"sdp_type"`
+	SDPType   string `json:"sdp_type"` // DEPRECATED - will be removed (it is always "offer")
 	SDP       string `json:"sdp"`
 	AgentID   string `json:"agent_id"`   // filled by the Wabaman server, not the client
 	AgentName string `json:"agent_name"` // filled by the Wabaman server, not the client
