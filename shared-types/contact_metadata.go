5591f936c51ad25cbbe8e7e0034703485cc5bb8f
Gabriel Ochsenhofer <gabriel.ochsenhofer@gmail.com>
2026-06-02 19:46:41 -0300

    gofmt

--- shared-types/contact_metadata.go
+++ shared-types/contact_metadata.go
@@ -6,37 +6,37 @@ import (
 )
 
 type ContactMetadata struct {
-	InquiryID                       *string                  `json:"inquiry_id,omitzero"`
-	InquiryStatus                   InquiryStatus            `json:"inquiry_status,omitzero"`
-	InquiryDisplayID                *string                  `json:"inquiry_display_id,omitzero"`
-	InquiryAgentID                  *string                  `json:"inquiry_agent_id,omitzero"`
-	InquiryAgentName                *string                  `json:"inquiry_agent_name,omitzero"`
-	InquiryAIEvaluation             *string                  `json:"inquiry_ai_evaluation,omitzero"`
-	InquiryCanBindDisplayID         MetadataBool             `json:"inquiry_can_bind_display_id,omitzero"`
-	InquiryCreatedAt                *time.Time               `json:"inquiry_created_at,omitzero"`
-	InquiryExpireDate               *time.Time               `json:"inquiry_expire_date,omitzero"`
-	InquirySellerAgentID            *string                  `json:"inquiry_seller_agent_id,omitzero"`
-	InquirySellerAgentName          *string                  `json:"inquiry_seller_agent_name,omitzero"`
-	InquiryHasPendencies            MetadataBool             `json:"inquiry_has_pendencies,omitzero"`
-	InquirySellOpportunityCollected MetadataBool             `json:"inquiry_sell_opportunity_collected,omitzero"`
-	InquiryQuotatedAt               *time.Time               `json:"inquiry_quotated_at,omitzero"`
-	InquiryDoneAt                   *time.Time               `json:"inquiry_done_at,omitzero"`
-	InquiryLastStatusUpdate         *time.Time               `json:"inquiry_last_status_update,omitzero"`
-	InquiryIsChatOpen               MetadataBool             `json:"inquiry_is_chat_open,omitzero"`
-	InquiryIsMarketplace            MetadataBool             `json:"inquiry_is_marketplace,omitzero"`
-	InquiryInclusorAgentID          *string                  `json:"inquiry_inclusor_agent_id,omitzero"`
-	InquiryInclusorAgentName        *string                  `json:"inquiry_inclusor_agent_name,omitzero"`
-	InquirySpecialistAgentID        *string                  `json:"inquiry_specialist_agent_id,omitzero"`
-	InquirySpecialistAgentName      *string                  `json:"inquiry_specialist_agent_name,omitzero"`
-	AccountID                       *string                  `json:"account_id,omitzero"`
-	ChatbotDisabled                 MetadataBool             `json:"chatbot_disabled,omitzero"`
-	ChatbotInitialContact           *time.Time               `json:"chatbot_initial_contact,omitzero"`
-	ChatbotIsPreRegistration        MetadataBool             `json:"chatbot_is_pre_registration,omitzero"`
-	ChatbotLastState                ChatbotLastState         `json:"chatbot_last_state,omitzero"`
-	ChatbotRegistrationDate         *time.Time               `json:"chatbot_registration_date,omitzero"`
-	InitialContactChannel           InitialContactChannel    `json:"initial_contact_channel,omitzero"`
-	InitialContactDate              *time.Time               `json:"initial_contact_date,omitzero"`
-	OtherFields                     map[string]any           `json:"-"`
+	InquiryID                       *string               `json:"inquiry_id,omitzero"`
+	InquiryStatus                   InquiryStatus         `json:"inquiry_status,omitzero"`
+	InquiryDisplayID                *string               `json:"inquiry_display_id,omitzero"`
+	InquiryAgentID                  *string               `json:"inquiry_agent_id,omitzero"`
+	InquiryAgentName                *string               `json:"inquiry_agent_name,omitzero"`
+	InquiryAIEvaluation             *string               `json:"inquiry_ai_evaluation,omitzero"`
+	InquiryCanBindDisplayID         MetadataBool          `json:"inquiry_can_bind_display_id,omitzero"`
+	InquiryCreatedAt                *time.Time            `json:"inquiry_created_at,omitzero"`
+	InquiryExpireDate               *time.Time            `json:"inquiry_expire_date,omitzero"`
+	InquirySellerAgentID            *string               `json:"inquiry_seller_agent_id,omitzero"`
+	InquirySellerAgentName          *string               `json:"inquiry_seller_agent_name,omitzero"`
+	InquiryHasPendencies            MetadataBool          `json:"inquiry_has_pendencies,omitzero"`
+	InquirySellOpportunityCollected MetadataBool          `json:"inquiry_sell_opportunity_collected,omitzero"`
+	InquiryQuotatedAt               *time.Time            `json:"inquiry_quotated_at,omitzero"`
+	InquiryDoneAt                   *time.Time            `json:"inquiry_done_at,omitzero"`
+	InquiryLastStatusUpdate         *time.Time            `json:"inquiry_last_status_update,omitzero"`
+	InquiryIsChatOpen               MetadataBool          `json:"inquiry_is_chat_open,omitzero"`
+	InquiryIsMarketplace            MetadataBool          `json:"inquiry_is_marketplace,omitzero"`
+	InquiryInclusorAgentID          *string               `json:"inquiry_inclusor_agent_id,omitzero"`
+	InquiryInclusorAgentName        *string               `json:"inquiry_inclusor_agent_name,omitzero"`
+	InquirySpecialistAgentID        *string               `json:"inquiry_specialist_agent_id,omitzero"`
+	InquirySpecialistAgentName      *string               `json:"inquiry_specialist_agent_name,omitzero"`
+	AccountID                       *string               `json:"account_id,omitzero"`
+	ChatbotDisabled                 MetadataBool          `json:"chatbot_disabled,omitzero"`
+	ChatbotInitialContact           *time.Time            `json:"chatbot_initial_contact,omitzero"`
+	ChatbotIsPreRegistration        MetadataBool          `json:"chatbot_is_pre_registration,omitzero"`
+	ChatbotLastState                ChatbotLastState      `json:"chatbot_last_state,omitzero"`
+	ChatbotRegistrationDate         *time.Time            `json:"chatbot_registration_date,omitzero"`
+	InitialContactChannel           InitialContactChannel `json:"initial_contact_channel,omitzero"`
+	InitialContactDate              *time.Time            `json:"initial_contact_date,omitzero"`
+	OtherFields                     map[string]any        `json:"-"`
 }
 
 var contactMetadataKnownKeys = map[string]struct{}{
@@ -109,26 +109,26 @@ func (cm *ContactMetadata) UnmarshalJSON(data []byte) error {
 	}
 
 	known := map[string]any{
-		"inquiry_id":                         &cm.InquiryID,
-		"inquiry_display_id":                 &cm.InquiryDisplayID,
-		"inquiry_agent_id":                   &cm.InquiryAgentID,
-		"inquiry_agent_name":                 &cm.InquiryAgentName,
-		"inquiry_ai_evaluation":              &cm.InquiryAIEvaluation,
-		"inquiry_created_at":                 &cm.InquiryCreatedAt,
-		"inquiry_expire_date":                &cm.InquiryExpireDate,
-		"inquiry_seller_agent_id":            &cm.InquirySellerAgentID,
-		"inquiry_seller_agent_name":          &cm.InquirySellerAgentName,
-		"inquiry_quotated_at":                &cm.InquiryQuotatedAt,
-		"inquiry_done_at":                    &cm.InquiryDoneAt,
-		"inquiry_last_status_update":         &cm.InquiryLastStatusUpdate,
-		"inquiry_inclusor_agent_id":          &cm.InquiryInclusorAgentID,
-		"inquiry_inclusor_agent_name":        &cm.InquiryInclusorAgentName,
-		"inquiry_specialist_agent_id":        &cm.InquirySpecialistAgentID,
-		"inquiry_specialist_agent_name":      &cm.InquirySpecialistAgentName,
-		"account_id":                         &cm.AccountID,
-		"chatbot_initial_contact":            &cm.ChatbotInitialContact,
-		"chatbot_registration_date":          &cm.ChatbotRegistrationDate,
-		"initial_contact_date":               &cm.InitialContactDate,
+		"inquiry_id":                    &cm.InquiryID,
+		"inquiry_display_id":            &cm.InquiryDisplayID,
+		"inquiry_agent_id":              &cm.InquiryAgentID,
+		"inquiry_agent_name":            &cm.InquiryAgentName,
+		"inquiry_ai_evaluation":         &cm.InquiryAIEvaluation,
+		"inquiry_created_at":            &cm.InquiryCreatedAt,
+		"inquiry_expire_date":           &cm.InquiryExpireDate,
+		"inquiry_seller_agent_id":       &cm.InquirySellerAgentID,
+		"inquiry_seller_agent_name":     &cm.InquirySellerAgentName,
+		"inquiry_quotated_at":           &cm.InquiryQuotatedAt,
+		"inquiry_done_at":               &cm.InquiryDoneAt,
+		"inquiry_last_status_update":    &cm.InquiryLastStatusUpdate,
+		"inquiry_inclusor_agent_id":     &cm.InquiryInclusorAgentID,
+		"inquiry_inclusor_agent_name":   &cm.InquiryInclusorAgentName,
+		"inquiry_specialist_agent_id":   &cm.InquirySpecialistAgentID,
+		"inquiry_specialist_agent_name": &cm.InquirySpecialistAgentName,
+		"account_id":                    &cm.AccountID,
+		"chatbot_initial_contact":       &cm.ChatbotInitialContact,
+		"chatbot_registration_date":     &cm.ChatbotRegistrationDate,
+		"initial_contact_date":          &cm.InitialContactDate,
 	}
 
 	// Interned enum fields
